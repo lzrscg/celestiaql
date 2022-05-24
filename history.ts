02fed481ec23053e36779c333d7eeed35735d94a
LZRS <lzrscg@protonmail.com>
2022-05-24 00:19:49 -0700

    print blocks within a range

--- history.ts
+++ history.ts
@@ -4,7 +4,7 @@ import http from "http";
 import Block from "./block";
 
 class History {
-  constructor(private _mongoCollection: Collection) {
+  constructor(/*private _mongoCollection: Collection*/) {
     this._axiosInstance = axios.create({
       // tendermint can't handle many new http connections
       httpAgent: new http.Agent({ keepAlive: true }),
@@ -13,10 +13,10 @@ class History {
 
   private _axiosInstance: AxiosInstance;
 
-  public async syncFrom(latestBlockHeight: number) {
+  public async syncFrom(latestBlockHeight: number, until?: number) {
     for (
       let currentBlockNumber = latestBlockHeight;
-      currentBlockNumber > 0;
+      currentBlockNumber >= (until ?? 1);
       currentBlockNumber--
     ) {
       await this._sync(currentBlockNumber);
@@ -33,15 +33,16 @@ class History {
     const response: any = await this._axiosInstance.get(
       `http://localhost:26657/block?height=${blockHeight}`
     );
+    console.log(JSON.stringify(response.data, null, 4));
 
     // if there are no payForMessages, don't procees block
-    if (!Block.hasPfms(response.data)) {
+    /*if (!Block.hasPfms(response.data)) {
       return;
     }
     const block = Block.create(response.data);
     block.payForMessages.forEach((payForMessage) => {
       payForMessage.save(this._mongoCollection);
-    });
+    });*/
   }
 }
 
--- index.ts
+++ index.ts
@@ -1,28 +1,10 @@
 import History from "./history";
 
-const { MongoClient } = require("mongodb");
-// Connection URI
-const uri = "mongodb://localhost:27017/?maxPoolSize=20&w=majority";
-// Create a new MongoClient
-const client = new MongoClient(uri);
 async function run() {
-  try {
-    // Connect the client to the database
-    await client.connect();
+  const history = new History();
 
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to database");
+  await history.syncFrom(2407,2407);
 
-    const pfmCollection = client.db("celestiaql").collection("payForMessages");
-    const history = new History(pfmCollection);
-
-    await history.syncFrom(81830);
-
-    console.log("Done");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  console.log("Done");
 }
-run().catch(console.dir);
+run();
